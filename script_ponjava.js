885e22e463fce775061ac3ddba82a20eea2d28f0
Oskar-cmyk <oskar.kandy@gmail.com>
2024-05-27 08:33:11 +0200

    fix1

--- script_ponjava.js
+++ script_ponjava.js
@@ -75,14 +75,6 @@ function initializeGyroEffect() {
                         enableGyro();
                         requestPermissionButton.style.display = 'none'; // Hide the permission button
                         toggleButton.style.display = 'block'; // Show the toggle button
-                        toggleEffect(); // Enable the effect and change button text to "Razgrni"
-                        toggleButton.textContent = isMoving ? 'Preteguj' : 'Razgrni'; // Change the button text based on the state
-    
-                        // Event listener to handle button click and toggle the effect
-                        toggleButton.addEventListener('click', toggleEffect);
-    
-                        // Initial text content of the button based on the initial state of the effect
-                        toggleButton.textContent = isMoving ? 'Preteguj' : 'Razgrni';
                     } else {
                         alert('Permission to access gyroscope data was denied.');
                     }
@@ -93,12 +85,9 @@ function initializeGyroEffect() {
         // If no permission is required or it's an Android device, enable gyro directly
         enableGyro();
         toggleButton.style.display = 'block'; // Show the toggle button
-    
+        
         // Event listener to handle button click and toggle the effect
         toggleButton.addEventListener('click', toggleEffect);
-    
-        // Initial text content of the button based on the initial state of the effect
-        toggleButton.textContent = isMoving ? 'Preteguj' : 'Razgrni';
     }
 
     // Hide the toggle button initially on iPhones
